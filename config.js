adcee42bfc17b09366212c9588e3edcd89459aa5
Akers <dtphong010120@gmail.com>
2024-07-28 15:27:38 +0700

    first commit

---
+++ config.js
@@ -0,0 +1,19 @@
+// 1. Chỉnh sửa các dòng chữ theo mục đích của bạn
+const CONFIG = {
+    introTitle: ' Hee loo bạn nhỏ !!!',
+    introDesc: `Anh có điều muốn nói với em 😅`,
+    btnIntro: 'Tiếp theo',
+    title: 'Cho anh cơ hội làm lại nhé? 😢',
+    desc: 'Anh xin lỗi vì thời gian đó đã làm tổn thương em.\n Nên hiện tại anh muốn xin lỗi và cũng mong muốn một cơ hội để sửa chữa lại sai lầm của mình.'
+    +'Em tha thứ và cho anh một cơ hội nhé.',
+    btnYes: 'Đồng ý',
+    btnNo: 'Từ chối 😢 ',
+    question:'Gửi cho anh lời em muốn nói nhé',
+    reasonPlaceholder: 'Lời nói',
+    btnReply: 'Gửi ! ',
+    reply: 'Em đồng ý cho anh một cơ hội nứa. Đừng làm tổn thương em nứa nhé 😊😊',
+    mess: 'Cám ơn em vì đã cho anh cơ hội làm lại.',
+    messDesc: '💬💬 Nhắn tin cho Anh nha. 💬💬',
+    btnAccept: 'Okê la la 💖',
+    messLink: 'https://www.facebook.com/tuan.phong.16906715/'
+}
